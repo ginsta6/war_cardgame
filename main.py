c5c1a4925a6ad0920f374aadf5a03b67e80b7219
ginsta6 <gintazzz9@gmail.com>
2024-11-22 14:40:37 +0200

    inputs

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from game import Game
+import sys
 
 def main():
-    mygame = Game(True)
+    try:
+        if len(sys.argv) != 3:
+            sys.exit("\033[95m Specify deck size (F/H) and round quantity \033[0m")
+        else:
+            match sys.argv[1].lower():
+                case "f":
+                    mygame = Game(True, int(sys.argv[2]))
+                case "h":
+                    mygame = Game(False, int(sys.argv[2]))
+    except Exception as err:
+        print(type(err))
     mygame.game_loop()
 
 
